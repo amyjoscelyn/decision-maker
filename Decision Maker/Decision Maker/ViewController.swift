dea9b8ef597557f47df68398fcbc74a073f6351c
amyjoscelyn <amy.joscelyn@flatironschool.com>
2017-01-29 14:04:47 -0500

    xcode project begun, .gitignore updated

---
+++ Decision Maker/Decision Maker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Decision Maker
+//
+//  Created by Amy Joscelyn on 1/29/17.
+//  Copyright © 2017 Amy Joscelyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
